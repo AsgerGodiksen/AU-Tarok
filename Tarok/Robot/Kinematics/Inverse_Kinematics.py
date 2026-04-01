Fixed two sign errors in Inverse Kinematics

Tarok/Robot/Kinematics/Inverse_Kinematics.py
@@ -52,7 +52,7 @@ def Inverse_Kinematics(P_org, Leg):
     H = np.sqrt(x**2 + y**2 - 0.078**2)
 
     # Compute theta1
-    #theta1 = np.arctan2(config*y, x) - np.arctan2(0.078, H)
+    #theta1 = np.arctan2(config*y, x) - np.arctan2(0.078, H) ### OLD
     theta1 = config*(np.arctan2(config*y, x) - np.arctan2(0.078, H))
 
     # Determine constants needed
@@ -68,7 +68,8 @@ def Inverse_Kinematics(P_org, Leg):
     gamma = np.arctan2(-z, H)
 
     # Compute theta2
-    theta2 = gamma - beta
+    #theta2 = gamma - beta ### OLD
+    theta2 = config*gamma - beta
    
     # Check joint space limits
     if Leg == 'FL' or Leg == 'HR':

Tarok/Robot/Visualizations/Bezier_Gait.py
@@ -60,10 +60,10 @@ print(Bezier_Trajectory.shape)
 Front_Left_Shoulder, Front_Right_Shoulder, Hind_Left_Shoulder, Hind_Right_Shoulder = Tarok.Shoulder_Positions()
 print(Front_Left_Shoulder.shape)
 FL_Bezier_Trajectory = (Bezier_Trajectory + Front_Left_Shoulder) 
-FR_Bezier_Trajectory = Bezier_Trajectory * np.array([[-1], [1], [1]]) + Front_Right_Shoulder
-HL_Bezier_Trajectory = Bezier_Trajectory * np.array([[-1], [1], [1]]) + Hind_Left_Shoulder
-# FR_Bezier_Trajectory = (Bezier_Trajectory + Front_Right_Shoulder)
-# HL_Bezier_Trajectory = (Bezier_Trajectory + Hind_Left_Shoulder)  
+#FR_Bezier_Trajectory = Bezier_Trajectory * np.array([[-1], [1], [1]]) + Front_Right_Shoulder
+#HL_Bezier_Trajectory = Bezier_Trajectory * np.array([[-1], [1], [1]]) + Hind_Left_Shoulder
+FR_Bezier_Trajectory = (Bezier_Trajectory + Front_Right_Shoulder)
+HL_Bezier_Trajectory = (Bezier_Trajectory + Hind_Left_Shoulder)  
 HR_Bezier_Trajectory = (Bezier_Trajectory + Hind_Right_Shoulder) 
 
 